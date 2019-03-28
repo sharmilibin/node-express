c9575b248ec411be288318e3da04a2a13c7629c3
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-03-27 23:06:37 -0600

    removing console

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('content-type', 'text/html');
     res.end('<html><body><h1> This is Server Express </h1></body></html>');
