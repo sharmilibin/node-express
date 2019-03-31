5d59e9ddcadbd9bc002fc2ed3dea24be6770e8c2
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-03-30 22:06:59 -0600

    Adding new router end point for Promotions

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routers/dishRouter');
 const leaderRouter = require('./routers/leaderRouter');
+const promotionRouter = require('./routers/promotionRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 const app = express();
 app.use('/dishes',dishRouter);
 app.use('/leaders',leaderRouter);
+app.use('/promotions',promotionRouter);
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
